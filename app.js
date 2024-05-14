87f6229869b61cdd67ced88f74deb00894dec49f
“JesusHades” <jesusmanriquez56@aragon.unam.mx>
2024-05-14 08:04:54 -0600

    se agrega archivo js con funcion para leer el archivo csv

---
+++ app.js
@@ -0,0 +1,39 @@
+// --------------------  LEER ARCHIVO --------------------
+function parseCSV(csvData){
+
+    const rows = csvData.split('\n');
+    const tableBody = document.querySelector('#CsvTable tbody');
+
+    rows.forEach(row => {
+        const columns = row.split(',');
+        const tr = document.createElement('tr');
+
+        columns.forEach(column => {
+            const td = document.createElement('td');
+            td.textContent = column;
+            tr.appendChild(td);
+        });
+
+        tableBody.appendChild(tr);
+
+    });
+
+};
+
+
+function readCSV(file){
+    const reader = new FileReader();
+
+    reader.onload = function(e){
+        const csvData = e.target.result;
+        parseCSV(csvData);
+    };
+
+    reader.readAsText(file);
+};
+
+document.querySelector('input[type ="file"]').addEventListener('change', function(e){
+    const file = e.target.files[0];
+    readCSV(file);
+}); 
+// --------------------  LEER ARCHIVO --------------------
